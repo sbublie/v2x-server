fe993afe5ae2f50127ee2c24fcff966652dc8db3
sbublie <59267927+sbublie@users.noreply.github.com>
2022-01-25 20:51:27 +0900

    Formatting

--- message_service.py
+++ message_service.py
@@ -1,6 +1,7 @@
 import models
 import time
 
+
 class MessageService():
 
     def __init__(self, udp_service):
@@ -18,10 +19,11 @@ class MessageService():
                     if str(message.intersection_id) == str(intersection_id):
                         message.map_available = True
                     else:
-                        messages.append(models.Message(intersection_id, True, False))
-            else: 
+                        messages.append(models.Message(
+                            intersection_id, True, False))
+            else:
                 messages.append(models.Message(intersection_id, True, False))
-        
+
         for spat in spat_messages:
             intersection_id = spat['spat']['intersections'][0]['id']['id']
             if messages:
@@ -29,57 +31,58 @@ class MessageService():
                     if str(message.intersection_id) == str(intersection_id):
                         message.spat_available = True
                     else:
-                        messages.append(models.Message(intersection_id, False, True))
+                        messages.append(models.Message(
+                            intersection_id, False, True))
             else:
                 messages.append(models.Message(intersection_id, False, True))
-        
 
         return messages
 
     def get_intersection(self, intersection_id):
-        
+
         map = self.get_single_map(intersection_id)
         spat = self.get_single_spat(intersection_id)
 
         if "speedLimits" in map['map']['intersections'][0]:
-            speed_limits=[models.SpeedLimit(
-                speed = map['map']['intersections'][0]['speedLimits'][0]['speed'],
-                type = map['map']['intersections'][0]['speedLimits'][0]['type'])
-                ]
+            speed_limits = [models.SpeedLimit(
+                speed=map['map']['intersections'][0]['speedLimits'][0]['speed'],
+                type=map['map']['intersections'][0]['speedLimits'][0]['type'])
+            ]
         else:
             speed_limits = None
-        
-        ref_position = models.Position(map['map']['intersections'][0]['refPoint']['lat'], map['map']['intersections'][0]['refPoint']['long'])
-        
+
+        ref_position = models.Position(
+            map['map']['intersections'][0]['refPoint']['lat'], map['map']['intersections'][0]['refPoint']['long'])
+
         return models.Intersection(
-            id=map['map']['intersections'][0]['id']['id'], 
-            station_id = map['header']['stationID'],
-            name = map['map']['intersections'][0]['name'],
-            region=map['map']['intersections'][0]['id']['region'], 
-            revision=map['map']['intersections'][0]['revision'], 
-            status = None, 
-            moy = None,
-            timestamp = None,
-            lane_width = None,
-            speed_limits = speed_limits,
-            ref_position = ref_position,
-            lanes = self.get_lanes(intersection_id),
-            signal_groups = self.get_signal_groups(intersection_id)
+            id=map['map']['intersections'][0]['id']['id'],
+            station_id=map['header']['stationID'],
+            name=map['map']['intersections'][0]['name'],
+            region=map['map']['intersections'][0]['id']['region'],
+            revision=map['map']['intersections'][0]['revision'],
+            status=None,
+            moy=None,
+            timestamp=None,
+            lane_width=None,
+            speed_limits=speed_limits,
+            ref_position=ref_position,
+            lanes=self.get_lanes(intersection_id),
+            signal_groups=self.get_signal_groups(intersection_id)
         )
 
-    
     def get_lanes(self, intersection_id):
         single_map = self.get_single_map(intersection_id)
-        
+
         lanes = []
 
         for lane in single_map['map']['intersections'][0]['laneSet']:
-    
+
             nodes = []
             # Why 1 here?
             for node in lane['nodeList'][1]:
-                nodes.append(models.Node(models.Offset(node['delta'][1]['x'], node['delta'][1]['y'])))
-            
+                nodes.append(models.Node(models.Offset(
+                    node['delta'][1]['x'], node['delta'][1]['y'])))
+
             ingress_approach_id, egress_approach_id, approach_type, shared_with_id, maneuver_id = None, None, 0, None, None
 
             if 'ingressApproach' in lane:
@@ -89,14 +92,15 @@ class MessageService():
             if 'directionalUse' in lane['laneAttributes']:
                 if lane['laneAttributes']['directionalUse'][0] == b'\x80':
                     approach_type = 1
-            #if 'sharedWith' in lane['laneAttributes']:
+            # if 'sharedWith' in lane['laneAttributes']:
                 #shared_with_id = lane['laneAttributes']['sharedWith']
-            #if 'maneuvers' in lane['laneAttributes']:
+            # if 'maneuvers' in lane['laneAttributes']:
                 #maneuver_id = lane['laneAttributes']['maneuvers']
-                
+
             connects_to = None
             if 'connectsTo' in lane and 'signalGroup' in lane['connectsTo'][0]:
-                connects_to = models.ConnectsTo(signal_group_id=lane['connectsTo'][0]['signalGroup'])
+                connects_to = models.ConnectsTo(
+                    signal_group_id=lane['connectsTo'][0]['signalGroup'])
 
             lane_type = None
             if 'vehicle' in lane['laneAttributes']['laneType']:
@@ -104,7 +108,7 @@ class MessageService():
             if 'bikeLane' in lane['laneAttributes']['laneType']:
                 lane_type = 1
             if 'crosswalk' in lane['laneAttributes']['laneType']:
-                lane_type = 2 
+                lane_type = 2
 
             lanes.append(models.Lane(
                 id=lane['laneID'],
@@ -122,21 +126,24 @@ class MessageService():
     def get_signal_groups(self, intersection_id):
         spat = self.get_single_spat(intersection_id)
         signal_groups = []
-        
+
         if spat:
             for signal_group in spat['spat']['intersections'][0]['states']:
-                
+
                 min_end_time, max_end_time, likely_time, confidence = None, None, None, None
 
                 if 'minEndTime' in signal_group['state-time-speed'][0]['timing']:
-                    min_end_time = self.convertTime(signal_group['state-time-speed'][0]['timing']['minEndTime'])
-                
+                    min_end_time = self.convertTime(
+                        signal_group['state-time-speed'][0]['timing']['minEndTime'])
+
                 if 'maxEndTime' in signal_group['state-time-speed'][0]['timing']:
-                    max_end_time = self.convertTime(signal_group['state-time-speed'][0]['timing']['maxEndTime'])
-                
+                    max_end_time = self.convertTime(
+                        signal_group['state-time-speed'][0]['timing']['maxEndTime'])
+
                 if 'likelyTime' in signal_group['state-time-speed'][0]['timing']:
-                    likely_time = self.convertTime(signal_group['state-time-speed'][0]['timing']['likelyTime'])
-                
+                    likely_time = self.convertTime(
+                        signal_group['state-time-speed'][0]['timing']['likelyTime'])
+
                 if 'confidence' in signal_group['state-time-speed'][0]['timing']:
                     confidence = signal_group['state-time-speed'][0]['timing']['confidence']
 
@@ -152,15 +159,15 @@ class MessageService():
                     state = 4
 
                 new_state = models.SignalGroup(
-                    id = signal_group['signalGroup'],
-                    state = state,
-                    min_end_time = min_end_time,            
-                    max_end_time = max_end_time,
-                    likely_time = likely_time,
-                    confidence = confidence
+                    id=signal_group['signalGroup'],
+                    state=state,
+                    min_end_time=min_end_time,
+                    max_end_time=max_end_time,
+                    likely_time=likely_time,
+                    confidence=confidence
                 )
                 signal_groups.append(new_state)
-        
+
         return signal_groups
 
     def get_single_spat(self, intersection_id):
@@ -168,7 +175,7 @@ class MessageService():
         for spat in spat_messages:
             if str(intersection_id) == str(spat['spat']['intersections'][0]['id']['id']):
                 return spat
-    
+
     def get_single_map(self, intersection_id):
         map_messages = self.udp_service.map_responses
         for map in map_messages:
@@ -178,7 +185,8 @@ class MessageService():
     def convertTime(self, given_seconds):
 
         result = time.localtime(time.time())
-        t = (result.tm_year, result.tm_mon, result.tm_mday, result.tm_hour, 0, 0, 0, 0, 0)
+        t = (result.tm_year, result.tm_mon,
+             result.tm_mday, result.tm_hour, 0, 0, 0, 0, 0)
         lt = time.mktime(t)
-        #return time.ctime(lt+(int(given_seconds)/10))
+        # return time.ctime(lt+(int(given_seconds)/10))
         return 123456
--- models.py
+++ models.py
@@ -14,11 +14,13 @@ class Intersection():
         self.lanes = lanes
         self.signal_groups = signal_groups
 
+
 class Message():
     def __init__(self, intersection_id, map_available, spat_available):
         self.intersection_id = intersection_id
         self.map_available = map_available
-        self.spat_available =  spat_available
+        self.spat_available = spat_available
+
 
 class SignalGroup():
     def __init__(self, id, state=None, min_end_time=None, max_end_time=None, likely_time=None, confidence=None) -> None:
@@ -28,6 +30,7 @@ class SignalGroup():
         self.max_end_time = max_end_time
         self.likely_time = likely_time
         self.confidence = confidence
+
 
 class Lane():
     def __init__(self, id, type, nodes=None, connects_to=None, ingress_approach_id=None, egress_approach_id=None, approach_type=None, shared_with_id=None, maneuver_id=None) -> None:
@@ -41,26 +44,31 @@ class Lane():
         self.shared_with_id = shared_with_id
         self.maneuver_id = maneuver_id
 
+
 class Node():
     def __init__(self, offset, d_width=None) -> None:
         self.offset = offset
         self.d_width = d_width
+
 
 class Offset():
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
 
+
 class ConnectsTo():
-    def __init__(self, lane_id = None, maneuver = None, signal_group_id = None) -> None:
+    def __init__(self, lane_id=None, maneuver=None, signal_group_id=None) -> None:
         self.lane = lane_id
         self.maneuver = maneuver
         self.signal_group_id = signal_group_id
+
 
 class Position():
     def __init__(self, lat, long) -> None:
         self.lat = lat
         self.long = long
+
 
 class SpeedLimit():
     def __init__(self, speed, type):
